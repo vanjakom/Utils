47a61d780d43fba3454e8546735e92f226bb7e0b
Vanja Komadinovic <vanjakom@gmail.com>
2011-09-20 19:29:10 +0200

    Directory structure and test TerminalResizer

---
+++ JavaImageResize/bin/image_resize.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp target/JavaImageResize-*-jar-with-dependencies.jar com.busywait.javaimageresize.TerminalResizer $@
---
+++ JavaImageResize/src/main/java/com/busywait/javaimageresize/TerminalResizer.java
@@ -0,0 +1,16 @@
+package com.busywait.javaimageresize;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vanja
+ * Date: 9/20/11
+ * Time: 15:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class TerminalResizer {
+    public static void main(String[] args) {
+        for (String arg: args) {
+            System.out.println("Arg: " + arg);
+        }
+    }
+}
