540b4381b50eea6136e1d8dbce3991ae5bf9ded7
Vanja Komadinovic <vanjakom@gmail.com>
2011-09-21 17:51:13 +0200

    beta code, everything written, requires testing and more docs

--- JavaImageResize/bin/image_resize.sh
+++ JavaImageResize/bin/image_resize.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-java -cp target/JavaImageResize-*-jar-with-dependencies.jar com.busywait.javaimageresize.TerminalResizer $@
+DESTINATION_FOLDER = /Users/vanja/Desktop/resized/
+GREATER_DIMENSION = 800;
+java -cp target/JavaImageResize-*-jar-with-dependencies.jar com.busywait.javaimageresize.TerminalResizer $@ DESTINATION_FOLDER GREATER_DIMENSION
--- JavaImageResize/src/main/java/com/busywait/javaimageresize/TerminalResizer.java
+++ JavaImageResize/src/main/java/com/busywait/javaimageresize/TerminalResizer.java
@@ -1,5 +1,12 @@
 package com.busywait.javaimageresize;
 
+import net.coobird.thumbnailator.Thumbnails;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 /**
  * Created by IntelliJ IDEA.
  * User: vanja
@@ -11,6 +18,45 @@ public class TerminalResizer {
     public static void main(String[] args) {
         for (String arg: args) {
             System.out.println("Arg: " + arg);
+        }
+
+        String sourcePath = null;
+        String destinationFolder = null;
+        int greaterDimension = 800;
+
+        try {
+            sourcePath = args[0];
+            destinationFolder = args[1];
+            greaterDimension = Integer.parseInt(args[2]);
+        } catch (Exception e) {
+            System.err.println("Program requires 3 arguments: source file, destination folder and greater dimension");
+            e.printStackTrace();
+        }
+
+        File sourceFile = new File(sourcePath);
+        new File(destinationFolder).mkdirs();
+
+        try {
+            ImageIcon imageIcon = new ImageIcon(sourcePath);
+            Image image = imageIcon.getImage();
+            BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
+                    image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+
+            int resizedWidth = 0;
+            int resizedHeight = 0;
+
+            if (bufferedImage.getWidth() > bufferedImage.getHeight()) {
+                resizedWidth = greaterDimension;
+                resizedHeight = bufferedImage.getHeight() * resizedWidth / bufferedImage.getWidth();
+            } else {
+                resizedHeight = greaterDimension;
+                resizedWidth = bufferedImage.getWidth() * resizedHeight / bufferedImage.getHeight();
+            }
+
+            Thumbnails.of(bufferedImage).size(resizedWidth, resizedHeight).toFile(destinationFolder + "/" + sourceFile.getName());
+        } catch (Exception e)  {
+            System.err.println("Unable to resize image: " + sourcePath);
+            e.printStackTrace();
         }
     }
 }
